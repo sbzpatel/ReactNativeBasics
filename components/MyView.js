16bfb886ae1c908d9153e113185fdfb2e8575206
Shahbaz Patel <shahbazpatel1989@gamil.com>
2025-01-14 18:40:09 +0530

    changes in View file

--- components/MyView.js
+++ components/MyView.js
@@ -4,7 +4,7 @@ import externalStyle from './externalStyle'
 
 const MyView = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "pink" }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "hotpink" }}>
       <Text style={{...externalStyle.heading, color: "#535955"}}>MyView</Text>
     </View>
   )
